3837571d9c1c76cc354204aec287e48fd29b90a5
Terrence Y. Zhang <46796143+tzhangwps@users.noreply.github.com>
2019-08-25 14:44:34 -0400

    Update RecessionPredictor_paths.py

--- RecessionPredictor_paths.py
+++ RecessionPredictor_paths.py
@@ -6,7 +6,7 @@ import datetime as dt
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))   
 
-fred_api_key = 'a4ac36498b855f9f9f786164f19e1688'
+fred_api_key = ''
 now = dt.datetime.now()
 month = now.strftime('%m')
 year = now.year
